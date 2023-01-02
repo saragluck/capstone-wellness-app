d6aaa7bd9c52a3ebd7e8ec3995a7f5539481c34f
Sara Gluck <srfgglck@gmail.com>
2023-01-02 13:42:00 -0600

    Adds bcrypt gem and validations to the user

--- app/models/water.rb
+++ app/models/water.rb
@@ -1,2 +1,3 @@
 class Water < ApplicationRecord
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_02_190005) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_02_190154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "goals", force: :cascade do |t|
+    t.string "goal"
+    t.string "category"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sleeps", force: :cascade do |t|
     t.time "asleep"
