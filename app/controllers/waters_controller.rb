a9c1016cebec1336e2deda4afce3a10f035eff8c
Sara Gluck <srfgglck@gmail.com>
2023-01-02 15:44:15 -0600

    adds water routes

--- app/controllers/waters_controller.rb
+++ app/controllers/waters_controller.rb
@@ -1,3 +1,18 @@
 class WatersController < ApplicationController
   before_action :authenticate_user
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+  end
 end
