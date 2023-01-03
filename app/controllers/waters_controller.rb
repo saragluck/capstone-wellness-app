b26eda6bdeb3b4a97917a01426067f99b6d1b31f
Sara Gluck <srfgglck@gmail.com>
2023-01-03 11:31:58 -0600

    Adds waters crud actions

--- app/controllers/waters_controller.rb
+++ app/controllers/waters_controller.rb
@@ -2,17 +2,42 @@ class WatersController < ApplicationController
   before_action :authenticate_user
 
   def create
+    water = Water.create(
+      user_id: current_user.id,
+      time: params[:time],
+      date: params[:date],
+      amount: params[:amount],
+    )
+    render json: water.as_json
   end
 
-  def show
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
+  #Index action
   def index
+    waters = Water.all
+    render json: waters.as_json
+  end
+
+  #Show action
+  def show
+    water = Water.find_by(id: params[:id])
+    render json: water.as_json
+  end
+
+  #Update action
+  def update
+    water = Water.find_by(id: params[:id])
+    water.update(
+      time: params[:time] || water.time,
+      date: params[:date] || water.date,
+      amount: params[:amount] || water.amount,
+    )
+    render json: water.as_json
+  end
+
+  #destroy action
+  def destroy
+    water = Water.find_by(id: params[:id])
+    water.destroy
+    render json: { message: "water destroyed" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,11 @@ Rails.application.routes.draw do
   post "/sleeps" => "sleeps#create"
   patch "/sleeps/:id" => "sleeps#update"
   delete "/sleeps/:id" => "sleeps#destroy"
+
+  # Water actions
+  get "/waters" => "waters#index"
+  get "/waters/:id" => "waters#show"
+  post "/waters" => "waters#create"
+  patch "/waters/:id" => "waters#update"
+  delete "/waters/:id" => "waters#destroy"
 end
