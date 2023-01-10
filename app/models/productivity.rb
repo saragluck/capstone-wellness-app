db6813dbb9ff4c52400346d4d850a02735a679ac
Sara Gluck <srfgglck@gmail.com>
2023-01-09 23:13:24 -0600

    makes productivity model

---
+++ app/models/productivity.rb
@@ -0,0 +1,2 @@
+class Productivity < ApplicationRecord
+end
--- db/migrate/20230110051309_create_productivities.rb
+++ db/migrate/20230110051309_create_productivities.rb
@@ -2,8 +2,9 @@ class CreateProductivities < ActiveRecord::Migration[7.0]
   def change
     create_table :productivities do |t|
       t.integer :level
-      t.date :date
       t.time :time
+      t.date :date
+      t.integer :user_id
 
       t.timestamps
     end
