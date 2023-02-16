1b4719e67ac0f1f161bdd931bbd28840265e6d1c
saragluck <srfgglck@gmail.com>
2023-02-15 18:16:57 -0600

    fixes productivity database migrations

--- db/migrate/20230109203257_add_user_id_to_productivity.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToProductivity < ActiveRecord::Migration[7.0]
-  def change
-    add_column :productivities, :user_id, :integer
-  end
-end
--- db/migrate/20230110051022_drop_productivity.rb
+++
@@ -1,5 +0,0 @@
-class DropProductivity < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :productivities
-  end
-end
